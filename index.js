6a70d89cf2931146adf4e20f66f3c85a732b26fe
Vikram Chatterjee <strycora@gmail.com>
2021-02-24 14:03:17 -0700

    commit tree f1d5949f7160370a19b34594c68a4b0a42cf21ef

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map( line => {
+    const words = line.split(' ');
+    const capitalizedWords = 
+      words.map( word => word.charAt(0).toUpperCase() + word.slice(1) );
+    const response = capitalizedWords.join(' ');
+    return response;
+  })
 }
